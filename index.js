3dd244e945501232174cb1db6ed79f3058f570c2
alisoncassisn <alisoncassisn@gmail.com>
2017-10-10 22:04:45 -0300

    voltando mlab e retirando host

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const modules = require('./modules')
 const mlabURL = `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASSWORD}@ds163294.mlab.com:63294/rep-solar`
 
 server.connection({
-    host: process.env.HOST || 'localhost',
     port: process.env.PORT || 3000
 })
 
@@ -22,14 +21,14 @@ server.views({
     isCached: false
 })
 
-// db.connect(mlabURL, err => {
-//     if (err) {
-//         console.log('Unable to connect to Mlab')
-//         process.exit(1)
-//     }
-//     console.log('connected to Mlab')
+db.connect(mlabURL, err => {
+    if (err) {
+        console.log('Unable to connect to Mlab')
+        process.exit(1)
+    }
+    console.log('connected to Mlab')
     server.start(err => {
         if (err) console.log(err)
         console.log(`server running on PORT ${server.info.uri}`)
     })
-// })
+})
